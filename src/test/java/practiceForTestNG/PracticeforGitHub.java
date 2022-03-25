cfdb22f0ebb55f7b1b16b908ad4dab4cab05bb32
Lenovo <Lenovo@Aman-Anand>
2022-03-25 10:16:56 +0530

    Uodated from user1

---
+++ src/test/java/practiceForTestNG/PracticeforGitHub.java
@@ -0,0 +1,11 @@
+package practiceForTestNG;
+
+import org.testng.annotations.Test;
+
+public class PracticeforGitHub {
+	@Test
+	public void githubcreated() {
+		System.out.println("GitHubPractice");
+	}
+
+}
